52743c1f3f3ca4f635307f207f08c4d623af110a
Sunil Kumar Kandakatla <Sunil.Kandakatla@ATMECS.CORP>
2021-06-03 17:09:30 +0530

    first commit

---
+++ src/test/java/com/kandakatla/SpringBootPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kandakatla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
